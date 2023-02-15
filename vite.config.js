8792e5e22fd76522260a99e660ba26d747468f8d
Nikola Stankovic <jonnywintech@gmail.com>
2023-02-15 19:18:50 +0100

    images should be working now

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,10 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { copy } from "vite-plugin-copy";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), copy([{ src: 'src/assets', dest: 'dist/src' }])],
   server: {
     watch: {
       usePolling: true,
@@ -11,5 +12,8 @@ export default defineConfig({
     host: true, // needed for the Docker Container port mapping to work
     strictPort: true,
     port: 5173, // you can replace this port with any port
+  },
+  build: {
+    outDir: 'dist',
   },
 });
